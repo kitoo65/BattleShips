45fb7ed422afa481c3971d3c207702836d60a659
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2022-07-09 20:51:46 -0300

    Joystick Movement.
    Blocking keyboard input. Only touch movement.

--- Assets/Code/Ship.cs
+++ Assets/Code/Ship.cs
@@ -6,6 +6,7 @@ namespace Ships
     public class Ship : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private Joystick _joystick; // Creo una variable que almacena el boton de la UI.    
         private Transform _myTransform;
         private Camera _camera;
 
@@ -25,6 +26,12 @@ namespace Ships
             //Busco un vector2 que guarde los bordes de la camara. De 0 a 100%. 
             ClampFinalPosition();
         }
+        //Que problemas presenta esta solucion? Funciona bien. Pero si mañana, en vez de querer movernos con el axis
+        //Queremos movernos pulsando la pantalla.... Esta solucion, no valdria porque tenemos que cambiar el GetDirection, 
+        //Para utilizar los inputs de la pantalla. 
+        //O, por ejemplo que el GetDirection lo lea una inteligencia artificial.
+        //Puedo poner ifs, para hacer la implementacion, puedo duplicar los metodos para cada input, etc.
+        //Existe un patron: Patron Adapter.
 
         private void ClampFinalPosition()
         {
@@ -36,10 +43,15 @@ namespace Ships
 
         private Vector2 GetDirection()
         {
+
+            return new Vector2(_joystick.Horizontal,_joystick.Vertical); // Aqui, se anulan los inputs del teclado y anda solo con la UI.
+            //Acordarse tmb de referenciar la variable de tipo joystick en el editor.
             var horizontalDir = Input.GetAxis("Horizontal");
             var verticalDir = Input.GetAxis("Vertical");
             return new Vector2(horizontalDir,verticalDir);
         }
+        //DESVENTAJAS:
+        //No puedo jugar en PC. Tengo que ver si utilizo ifs, u optar de aplicar el patron adapter.
 
     }
 
