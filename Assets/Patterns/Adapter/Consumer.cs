3865c5f581f864ef022579e9b10ee0acc60f5584
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2022-07-09 22:20:02 -0300

    GetData Example.
    After we make the SetData implementation, we do the GetData, looking for the file, then converting it to JSON and then convert it to the Data Type for unity.

--- Assets/Patterns/Adapter/Consumer.cs
+++ Assets/Patterns/Adapter/Consumer.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
+using System;
+
 namespace Patterns.Adapter
 {
     //3, creamos un consumidor, para nuestra prueba.
     public class Consumer : MonoBehaviour
     {
+
+        private FileDataStoreAdapter _fileDataStoreAdapter;
         //3 El consumidor, busca los datos, se crea una "data" a guardar, 
         //Y luego al recibir con el SetData, se serializa en un JSOn, 
         //Y el json se transforma en un archivo.
         private void Awake()
         {
-            var fileDataStoreAdapter = new FileDataStoreAdapter();
+            _fileDataStoreAdapter = new FileDataStoreAdapter();
             var data = new Data("Dato 1", 123);
-            fileDataStoreAdapter.SetData(data, "Data1");
+            _fileDataStoreAdapter.SetData(data,"Data1");
+
+        }
+
+        private void Start()
+        {
+            //4. El consumidor tambien el start, busca la informacion del fichero.
+            var data = _fileDataStoreAdapter.GetData<Data>("Data1");
+            Debug.Log(data.Dato1);
+            Debug.Log(data.Dato2);
 
         }
     }
--- Assets/Patterns/Adapter/FileDataStoreAdapter.cs
+++ Assets/Patterns/Adapter/FileDataStoreAdapter.cs
@@ -6,21 +6,27 @@ namespace Patterns.Adapter
     //Creamos una implementacion de la interfaz.   1
     public class FileDataStoreAdapter : DataStore
     {
-        //En el GetData, busco un fichero con un nombre, convertire a json, luego a mi clase.
-        public T GetData<T>(string name)
-        {
-            throw new System.NotImplementedException();
-        }
-
         //Para SetData, creare un fichero con un nombre, y se serializaran los datos a JSON. 
         //Luego los guardo en el fichero.
         public void SetData<T>(T data, string name)
-        { 
-           var json =  JsonUtility.ToJson(data);
+        {
+            var json = JsonUtility.ToJson(data);
             //dataPath, es la carpeta Assets. 
-           var path = Path.Combine(Application.dataPath, name);
+            var path = Path.Combine(Application.dataPath, name);
 
-            File.WriteAllText(name,json);
+            File.WriteAllText(path, json);
         }
+        //En el GetData, busco un fichero con un nombre, convertire a json, luego a mi clase.
+        public T GetData<T>(string name)
+        {
+            //4 . Luego yo nuevamente busco el archivo, lo convierto a json, y lo devuelvo.
+            //Ir al consumer. 4
+            var path = Path.Combine(Application.dataPath, name);
+            var json = File.ReadAllText(path);
+            return JsonUtility.FromJson<T>(json);
+
+        }
+
+      
     }
 }
