8e99144692041acfedbe025cd1a30c90f983148d
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2022-07-11 16:01:33 -0300

    Implementing the PlayerPrefs method.
    See that sometimes parameters do make differences. There is the player prefs excercise for Adapter patterns.

--- Assets/Patterns/Adapter/Consumer.cs
+++ Assets/Patterns/Adapter/Consumer.cs
@@ -11,8 +11,14 @@ namespace Patterns.Adapter
         //3 El consumidor, busca los datos, se crea una "data" a guardar, 
         //Y luego al recibir con el SetData, se serializa en un JSOn, 
         //Y el json se transforma en un archivo.
+
+        PlayerPrefsAdapter _playerPrefsAdapter;
+
         private void Awake()
         {
+            _playerPrefsAdapter = new PlayerPrefsAdapter();
+            var data2 = new Data("Dato 2", 456);
+            _playerPrefsAdapter.SetData(data2, "Data2");
             _fileDataStoreAdapter = new FileDataStoreAdapter();
             var data = new Data("Dato 1", 123);
             _fileDataStoreAdapter.SetData(data,"Data1");
@@ -25,6 +31,10 @@ namespace Patterns.Adapter
             var data = _fileDataStoreAdapter.GetData<Data>("Data1");
             Debug.Log(data.Dato1);
             Debug.Log(data.Dato2);
+            var data2 = _playerPrefsAdapter.GetData<Data>("Data2");
+            Debug.Log(data2.Dato1);
+            Debug.Log(data2.Dato2);
+
 
         }
     }
